16077a6e744f1bac2e46797dcbabaa7b4b827aa9
Ahmed Kamal <info@ahmedmkamal.com>
2023-07-04 22:36:18 +0300

    Docs layout

--- src/app/docs/side-menu.tsx
+++ src/app/docs/side-menu.tsx
@@ -4,32 +4,32 @@ import { SideMenuItem } from "./side-menu-item";
 import { sideMenuItems } from "./side-menu-items";
 import styles from "./side-menu.module.scss";
 
-function SideMenu() {
+export function SideMenu() {
   return (
-    <Column scrollable gap={10} customClassName={styles.sideMenu}>
-      {sideMenuItems.map(({ icon: Icon, ...section }) => (
-        <Column key={section.title} gap={4}>
-          <Row gap={3} items="center">
-            <div className={styles.sectionIconContainer}>
-              <Icon size={14} />
-            </div>
-            <Text weight="semibold" color="basic">
-              {section.title}
-            </Text>
-          </Row>
-          <Column gap={4} customClassName="pl-2">
-            {section.links?.map((link) => (
-              <SideMenuItem
-                key={link.slug}
-                sectionSlug={section.slug}
-                link={link}
-              />
-            ))}
+    <Column customClassName={styles.sideMenu}>
+      <Column scrollable gap={10} customClassName={styles.contentContainer}>
+        {sideMenuItems.map(({ icon: Icon, ...section }) => (
+          <Column key={section.title} gap={4}>
+            <Row gap={3} items="center">
+              <div className={styles.sectionIconContainer}>
+                <Icon size={14} />
+              </div>
+              <Text weight="semibold" color="basic">
+                {section.title}
+              </Text>
+            </Row>
+            <Column gap={4} customClassName="pl-2">
+              {section.links?.map((link) => (
+                <SideMenuItem
+                  key={link.slug}
+                  sectionSlug={section.slug}
+                  link={link}
+                />
+              ))}
+            </Column>
           </Column>
-        </Column>
-      ))}
+        ))}
+      </Column>
     </Column>
   );
 }
-
-export default SideMenu;
